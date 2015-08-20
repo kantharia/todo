c0967bee4251d8c5f68eedaad945a5ed158289b0
Chetan Kantharia <chetan.kantharia@raweng.com>
2015-08-20 18:55:09 +0530

    Create Extension Hooks

--- server/app.js
+++ server/app.js
@@ -1,6 +1,33 @@
 // This line informs Extensions to use the latest version of SDK i.e. v2
 Built.Extension.setVersion(2);
-Built.Extension.define('verifyEmail', function(request, response) {
-	console.log('Email',request.params.email)
-  return response.success('Hello World');
+
+/*
+ Will trigger before saving object to class
+*/
+Built.Extension.beforeSave('tasks', function(request, response) {
+  //Check event-type for create and update
+  if(request.event === 'create' || request.event === 'update'){
+    var taskText = request.object.get('task_text');
+    console.error('taskText', taskText);
+    if(!taskText){
+      return response.error()
+    } else {
+      return response.success();
+    }
+  }
 });
+
+/*
+  Will trigger after saving object to class
+*/
+Built.Extension.afterSave('tasks', function(request, response) {
+  //Create an instance of Analytic Event
+  var tasksEvent = new Built.Analytics.Event('task_saved');
+  tasksEvent.trigger({
+    onSuccess: function() {
+      console.log('Task Created');
+      // successfully triggered
+      return response.success();
+    }
+  });
+})
